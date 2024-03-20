e463013f81960370bae0a52787cf1f16a7887382
cpb-work <cpb@bizb.ru>
2024-03-20 15:58:02 +0300

    add gh-pages branch for gh-pages

--- src/js/characters/__tests__/Magician.test.js
+++
@@ -1,7 +0,0 @@
-import Magician from '../Magician';
-
-test('creating Magician', () => {
-  const result = new Magician(1);
-  
-  expect(result).toEqual({health: 100, level: 1, attack: 10, defence: 40, type: 'magician'});
-});
--- src/js/characters/__tests__/Undead.test.js
+++
@@ -1,7 +0,0 @@
-import Undead from '../Undead';
-
-test('creating Undead', () => {
-  const result = new Undead(1);
-  
-  expect(result).toEqual({health: 100, level: 1, attack: 40, defence: 10, type: 'undead'});
-});
